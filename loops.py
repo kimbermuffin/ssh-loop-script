6de64270002eb4d63c6b525a33b69ceee9a0c20a
Kimberly Duke <kimber@kimbermcmuffin.local>
2016-05-24 18:58:04 -0700

    baby's first commit

---
+++ loops.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+
+import sys
+import time
+import select
+import paramiko
+
+hosts = ['192.168.7.10','192.168.50.10']
+
+# Connect to the hosts.
+
+for host in hosts:
+	print "Attempting communication with %s" % host
+	try:
+		ssh = paramiko.SSHClient()
+		ssh.load_system_host_keys()
+		ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+		ssh.connect(host, username='admin')
+		print "Connected to %s" % host
+		stdin, stdout, stderr = ssh.exec_command("ver")
+
+		# Wait for the command to terminate
+		while not stdout.channel.exit_status_ready():
+			# Only print data if there is data to read in the channel
+			if stdout.channel.recv_ready():
+				rl, wl, xl = select.select([stdout.channel], [], [], 0.0)
+				if len(rl) > 0:
+					# Print data from stdout
+					print stdout.channel.recv(1024),
+
+		#
+		# Disconnect from the host
+		#
+		print "Command done, closing SSH connection"
+		ssh.close()
+		#break
+
+	except paramiko.AuthenticationException:
+		print "Authentication failed when connecting to %s" % host
+		sys.exit(1)
+	except:
+		print "Could not SSH to %s, try again later." % host
+		sys.exit(1)
